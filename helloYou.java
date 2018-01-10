dd70cdde4fd0690f2730d8122394a5d5b870b4f0
LVBullock <laura.v.fisher@gmail.com>
2018-01-09 19:21:43 -0500

    Add files via upload

--- helloYou.java
+++ helloYou.java
@@ -10,12 +10,12 @@ public class helloYou {
 
     //Creating the main method
     public static void main(String[] args) {    //This line is the method header; text/logic between the {curly braces} is called the method body
-        System.out.println("Hello You");        //This line tells the program to print a line
+        System.out.println("Hello You.  How do you do?");        //This line tells the program to print a line
     }
     
 }
 /**
  * NOTE/COMMENTS:
  * - A method is like a recipe, provides steps/instructions
- * - You can type psvm>tab to quickly create the main method
+ * - tip:  You can type psvm>tab to quickly create the main method
  */
